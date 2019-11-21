7a309faa675c07abf7ad550c1d76ff25b3832044
sejal2803 <sejaljain2803@gmail.com>
2019-11-21 16:38:25 +0530

    initialized the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Layout from './component/Layout'
+import StepForm from './component/Form'
+
+function App() {
+  return (
+    <Container>
+      <Layout>
+        <StepForm />
+      </Layout>
+    </Container>
+  )
+}
+
+export default App
